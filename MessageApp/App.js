2ebfa70ac5fee211a977927482734509bc43c2aa
Nguyen Quang An <ngquangan@gmail.com>
2019-05-19 21:06:19 +0700

    Add google maps and render message list

--- MessageApp/App.js
+++ MessageApp/App.js
@@ -1,15 +1,37 @@
-import {
-  StyleSheet,
-  View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 
-import Status from './components/Status.js'
+import Status from "./components/Status.js";
+
+import MessageList from "./components/MessageList";
+import {
+  createImageMessage,
+  createLocationMessage,
+  createTextMessage
+} from "./utils/MessageUtils";
 
 const App = () => {
+  const [messages, setMessages] = React.useState([
+    createImageMessage("https://unsplash.it/300/300"),
+    createTextMessage("World"),
+    createTextMessage("Hello"),
+    createLocationMessage({
+      latitude: 16.078570199999998,
+      longitude: 108.14824949999999
+    })
+  ]);
+
+  const handlePressMessage = () => {}
 
   renderMessageList = () => {
-    return <View style={styles.content} />;
+    return (
+      <View style={styles.content}>
+        <MessageList
+          messages={messages}
+          onPressMessage={handlePressMessage}
+        />
+      </View>
+    );
   };
   renderInputMethodEditor = () => {
     return <View style={styles.inputMethodEditor} />;
---
+++ MessageApp/components/MessageList.js
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  FlatList, StyleSheet,
+  View, TouchableOpacity, Image, Text
+} from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+import PropTypes from "prop-types";
+import { MessageShape } from "../utils/MessageUtils";
+
+const keyExtractor = item => item.id.toString();
+
+const MessageList = ({ messages, onPressMessage = () => {} }) => {
+  renderMessageBody = ({ type, text, uri, coordinate }) => {
+    switch (type) {
+      case "text":
+        return (
+          <View style={styles.messageBubble}>
+            <Text style={styles.text}>{text}</Text>
+          </View>
+        );
+      case "image":
+        return <Image style={styles.image} source={{ uri }} />;
+      case "location":
+        return (
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+              ...coordinate,
+              latitudeDelta: 0.08,
+              longitudeDelta: 0.04
+            }}
+          >
+            <MapView.Marker coordinate={coordinate} />
+          </MapView>
+        );
+      default:
+        return null;
+    }
+  };
+
+  renderMessageItem = ({ item }) => {
+    return (
+      <View key={item.id} style={styles.messageRow}>
+        <TouchableOpacity onPress={() => onPressMessage(item)}>
+          {renderMessageBody(item)}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList
+      style={styles.container}
+      inverted // keo xuong duoi khi co message moi
+      data={messages}
+      renderItem={this.renderMessageItem}
+      keyExtractor={keyExtractor}
+      keyboardShouldPersistTaps={"handled"}
+    />
+  );
+};
+
+MessageList.propTypes = {
+  messages: PropTypes.arrayOf(MessageShape).isRequired,
+  onPressMessage: PropTypes.func
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: "visible"
+  },
+  messageRow: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginBottom: 4,
+    marginRight: 10,
+    marginLeft: 60
+  },
+  messageBubble: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "rgb(16,135,255)",
+    borderRadius: 20
+  },
+  text: {
+    fontSize: 18,
+    color: "#fff"
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 10
+  },
+  map: {
+    width: 250,
+    height: 250,
+    borderRadius: 10
+  }
+});
+
+export default MessageList;
