e950d406b81227cf96290ae2bf9a0a19a1d0163e
Nguyen Quang An <ngquangan@gmail.com>
2019-06-27 01:09:03 +0700

    Create Pie native code IOS

---
+++ ChartNativeModuleApp/ios/PieChart.m
@@ -0,0 +1,9 @@
+//
+//  “PieChart”.m
+//  ChartNativeModuleApp
+//
+//  Created by Nguyen Quang An on 5/25/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ChartNativeModuleApp/ios/PieChartManager.swift
@@ -0,0 +1,9 @@
+//
+//  PieChartManager.swift
+//  ChartNativeModuleApp
+//
+//  Created by Nguyen Quang An on 5/25/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
