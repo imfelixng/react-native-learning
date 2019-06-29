8790a26fd64bf95f816a54b67175d58486538fdf
Nguyen Quang An <ngquangan@gmail.com>
2019-06-29 15:28:32 +0700

    Create native code at ios and android

--- ChartNativeModuleApp/ios/PieChart.m
+++ ChartNativeModuleApp/ios/PieChart.m
@@ -7,3 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "React/RCTViewManager.h"
+
+@interface RCT_EXTERN_MODULE(PieChartManager, RCTViewManager)
+  RCT_EXPORT_VIEW_PROPERTY(data, NSArray)
+  RCT_EXPORT_VIEW_PROPERTY(strokeColor, UIColor)
+  RCT_EXPORT_VIEW_PROPERTY(strokeWidth, CGFloat)
+@end
