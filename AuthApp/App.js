ad530c01cca4a6d3c1574f19992697d2d1b6275e
Nguyen Quang An <ngquangan@gmail.com>
2019-07-09 22:43:48 +0700

    Init Auth App and add react native auth0

---
+++ AuthApp/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello React</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
